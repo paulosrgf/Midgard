066f5fb4a9b06d334352370d448b14d14ef81a4c
paulosrgf <paulosergiofilho@alunos.utfpr.edu.br>
2026-08-04 09:15:56 -0300

    chore(frontend): limpa boilerplate do Vue e configura Tailwind CSS v4

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+// Importe APENAS o arquivo onde você configurou o Tailwind
+import './style.css'
 
 const app = createApp(App)
 

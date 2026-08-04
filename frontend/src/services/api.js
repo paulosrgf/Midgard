00347f32a2514560da12f82d47408fb5a002530f
paulosrgf <paulosergiofilho@alunos.utfpr.edu.br>
2026-08-04 08:10:37 -0300

    refactor(frontend):updating api.js with vite api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  // O Vite expõe as variáveis de ambiente usando import.meta.env
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 })
 
 export default api

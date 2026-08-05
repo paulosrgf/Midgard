ab9000833301bcad9b55f4b33c3052cace846f16
paulosrgf <paulosergiofilho@alunos.utfpr.edu.br>
2026-08-05 08:34:03 -0300

    refactor(backend):adjusting the syntax for mysql migrations

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,15 +6,12 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+  
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            // Nossas novas colunas para o Instagram
             $table->string('username')->unique();
             $table->string('avatar')->nullable();
             $table->text('bio')->nullable();
